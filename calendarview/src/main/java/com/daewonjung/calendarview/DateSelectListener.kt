7beaf78465ae04d9128c0703932088ffe601256f
vulpes <teksu.lee@gmail.com>
2018-09-06 13:47:40 +0900

    Changed calendar view listener interface

--- calendarview/src/main/java/com/daewonjung/calendarview/DateSelectListener.kt
+++ calendarview/src/main/java/com/daewonjung/calendarview/DateSelectListener.kt
@@ -2,9 +2,7 @@ package com.daewonjung.calendarview
 
 interface DateSelectListener {
 
-    fun onDateSelected(date: CalendarDate)
-
-    fun onDateRangeSelected(start: CalendarDate, end: CalendarDate)
+    fun onSelectedDatesChanged(start: CalendarDate?, end: CalendarDate?)
 
     fun onSelectLimitDayExceed(start: CalendarDate, end: CalendarDate, selectLimitDay: Int)
 }
